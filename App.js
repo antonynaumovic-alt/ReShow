c2278b4ef432d3c7bbc03d57adfb761d6a2775c4
amymullins1 <19016241@brookes.ac.uk>
2021-01-25 10:24:13 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,238 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <body>
+        <img id="f33fd4f8b95b3e5806b865ff0d8e7f" src="f33fd4f8b95b3e5806b865ff0d8e7f.png"
+             srcSet="f33fd4f8b95b3e5806b865ff0d8e7f.png 1x, f33fd4f8b95b3e5806b865ff0d8e7f@2x.png 2x">
+        </img>
+            <svg className="Group_496">
+                <rect id="Group_496" rx="0" ry="0" x="0" y="0" width="1242" height="698">
+                </rect>
+            </svg>
+            <svg className="Rectangle_206_v">
+                <linearGradient id="Rectangle_206_v" spreadMethod="pad" x1="0.5" x2="0.5" y1="0" y2="1">
+                    <stop offset="0" stop-color="#000" stop-opacity="1"></stop>
+                    <stop offset="1" stop-color="#000" stop-opacity="0"></stop>
+                </linearGradient>
+                <rect id="Rectangle_206_v" rx="0" ry="0" x="0" y="0" width="1024" height="201">
+                </rect>
+            </svg>
+            <svg className="Rectangle_207_x">
+                <linearGradient id="Rectangle_207_x" spreadMethod="pad" x1="0.5" x2="0.5" y1="0" y2="1">
+                    <stop offset="0" stop-color="#000" stop-opacity="1"></stop>
+                    <stop offset="1" stop-color="#000" stop-opacity="0"></stop>
+                </linearGradient>
+                <rect id="Rectangle_207_x" rx="0" ry="0" x="0" y="0" width="1024" height="136">
+                </rect>
+            </svg>
+            <div id="Group_401">
+                <div id="OnlineLearning">
+                    <span>#OnlineLearning</span>
+                </div>
+                <div id="Online">
+                    <span>ONLINE</span>
+                </div>
+                <div id="Learning_Platform">
+                    <span>LEARNING PLATFORM</span>
+                </div>
+                <div id="ForAllAges">
+                    <span>Online Learning Platform for all ages.</span>
+                </div>
+            </div>
+            <div id="Group_2154">
+
+            </div>
+            <div id="Logo">
+                <svg className="ISU_Logo_2017-18_Transparent">
+                    <rect id="ISU_Logo_2017-18_Transparent" rx="65" ry="65" x="0" y="0" width="130" height="130">
+                    </rect>
+                </svg>
+                <div id="Temp_NAME">
+                    <span>Temp NAME</span>
+                </div>
+            </div>
+            <div id="In_page" className="In_page">
+                <svg className="Tap_zone">
+                    <rect id="Tap_zone" rx="0" ry="0" x="0" y="0" width="65" height="230">
+                    </rect>
+                </svg>
+                <svg className="Ellipse_427">
+                    <ellipse id="Ellipse_427" rx="5" ry="5" cx="5" cy="5">
+                    </ellipse>
+                </svg>
+                <svg className="Ellipse_427_">
+                    <ellipse id="Ellipse_427_" rx="2" ry="2" cx="2" cy="2">
+                    </ellipse>
+                </svg>
+                <svg className="Ellipse_427_ba">
+                    <ellipse id="Ellipse_427_ba" rx="2" ry="2" cx="2" cy="2">
+                    </ellipse>
+                </svg>
+                <svg className="Ellipse_427_bb">
+                    <ellipse id="Ellipse_427_bb" rx="2" ry="2" cx="2" cy="2">
+                    </ellipse>
+                </svg>
+                <svg className="Ellipse_427_bc">
+                    <ellipse id="Ellipse_427_bc" rx="2" ry="2" cx="2" cy="2">
+                    </ellipse>
+                </svg>
+                <svg className="Path_2472" viewBox="375.104 -94 1 28.761">
+                    <path id="Path_2472" d="M 375.1036987304688 -94 L 375.1036987304688 -65.2393798828125">
+                    </path>
+                </svg>
+                <svg className="Path_2473" viewBox="375.104 -94 1 28.761">
+                    <path id="Path_2473" d="M 375.1036987304688 -94 L 375.1036987304688 -65.2393798828125">
+                    </path>
+                </svg>
+                <svg className="Path_2474" viewBox="375.104 -94 1 28.761">
+                    <path id="Path_2474" d="M 375.1036987304688 -94 L 375.1036987304688 -65.2393798828125">
+                    </path>
+                </svg>
+                <svg className="Path_2475" viewBox="375.104 -94 1 28.761">
+                    <path id="Path_2475" d="M 375.1036987304688 -94 L 375.1036987304688 -65.2393798828125">
+                    </path>
+                </svg>
+                <svg className="Path_2476" viewBox="375.104 -94 1 28.761">
+                    <path id="Path_2476" d="M 375.1036987304688 -94 L 375.1036987304688 -65.2393798828125">
+                    </path>
+                </svg>
+                <div id="Group_2287">
+                    <div id="Intro">
+                        <span>Intro</span>
+                    </div>
+                    <div id="Features">
+                        <span>Features</span>
+                    </div>
+                    <div id="Mission">
+                        <span>Mission</span>
+                    </div>
+                    <div id="Application">
+                        <span>Application</span>
+                    </div>
+                    <div id="Why_Us">
+                        <span>Why Us?</span>
+                    </div>
+                    <div id="FAQ">
+                        <span>FAQ</span>
+                    </div>
+                </div>
+                <svg className="Ellipse_687">
+                    <ellipse id="Ellipse_687" rx="2" ry="2" cx="2" cy="2">
+                    </ellipse>
+                </svg>
+            </div>
+            <div id="Contact" className="Contact">
+                <svg className="Ellipse_432">
+                    <ellipse id="Ellipse_432" rx="10" ry="10" cx="10" cy="10">
+                    </ellipse>
+                </svg>
+                <div id="Contact_bs">
+                    <span>Contact</span>
+                </div>
+                <svg className="Polygon_16" viewBox="0 0 9 6">
+                    <path id="Polygon_16"
+                          d="M 3.700000047683716 1.066666722297668 C 4.099999904632568 0.5333333015441895 4.899999618530273 0.5333333015441895 5.300000190734863 1.066666722297668 L 7.800000190734863 4.400000095367432 C 8.294426918029785 5.059236526489258 7.824045658111572 6 7 6.000000476837158 L 2 6.000000476837158 C 1.175954580307007 6 0.7055727243423462 5.059236526489258 1.200000047683716 4.400000095367432 Z">
+                    </path>
+                </svg>
+                <div id="Group_2240">
+                    <svg className="Rectangle_241">
+                        <rect id="Rectangle_241" rx="1.164698839187622" ry="1.164698839187622" x="0" y="0"
+                              width="11.23" height="8.679">
+                        </rect>
+                    </svg>
+                    <svg className="Line_45" viewBox="0 0 3.548 3.195">
+                        <path id="Line_45" d="M 0 3.195338726043701 L 3.547592878341675 0">
+                        </path>
+                    </svg>
+                    <svg className="Line_46" viewBox="0 0 0.01 0.01">
+                        <path id="Line_46" d="M 0 0 L 0.01014393195509911 0.01014393195509911">
+                        </path>
+                    </svg>
+                    <svg className="Path_2488" viewBox="963.11 29.756 5.896 4.487">
+                        <path id="Path_2488"
+                              d="M 963.1099853515625 29.75600051879883 L 968.0186767578125 34.06608581542969 C 968.2520751953125 34.27129364013672 968.50390625 34.31425857543945 968.734375 34.10683059692383 L 969.0062255859375 33.8682861328125">
+                        </path>
+                    </svg>
+                    <svg className="Line_47" viewBox="0 0 2.641 2.641">
+                        <path id="Line_47" d="M 2.641258478164673 0 L 0 2.641258478164673">
+                        </path>
+                    </svg>
+                    <svg className="Line_48" viewBox="0 0 2.641 2.641">
+                        <path id="Line_48" d="M 0 0 L 2.641258478164673 2.641258478164673">
+                        </path>
+                    </svg>
+                </div>
+                <svg className="Ellipse_442">
+                    <ellipse id="Ellipse_442" rx="10" ry="10" cx="10" cy="10">
+                    </ellipse>
+                </svg>
+            </div>
+            <div id="Logins" className="Logins">
+                <svg className="Ellipse_433">
+                    <ellipse id="Ellipse_433" rx="10" ry="10" cx="10" cy="10">
+                    </ellipse>
+                </svg>
+                <div id="Logins_b">
+                    <span>Login</span>
+                </div>
+                <svg className="Polygon_15" viewBox="0 0 9 6">
+                    <path id="Polygon_15"
+                          d="M 3.700000047683716 1.066666722297668 C 4.099999904632568 0.5333333015441895 4.899999618530273 0.5333333015441895 5.300000190734863 1.066666722297668 L 7.800000190734863 4.400000095367432 C 8.294426918029785 5.059236526489258 7.824045658111572 6 7 6.000000476837158 L 2 6.000000476837158 C 1.175954580307007 6 0.7055727243423462 5.059236526489258 1.200000047683716 4.400000095367432 Z">
+                    </path>
+                </svg>
+                <div id="Group_2238">
+                    <svg className="Rectangle_240">
+                        <rect id="Rectangle_240" rx="1.7611846923828125" ry="1.7611846923828125" x="0" y="0"
+                              width="8.622" height="8.622">
+                        </rect>
+                    </svg>
+                    <svg className="Path_2487" viewBox="947.592 59.578 4.785 4.834">
+                        <path id="Path_2487"
+                              d="M 947.592041015625 64.41217803955078 L 947.592041015625 61.97113418579102 C 947.592041015625 60.64953231811523 948.66357421875 59.57799911499023 949.9852294921875 59.57799911499023 L 949.9852294921875 59.57799911499023 C 951.3056640625 59.57799911499023 952.377197265625 60.45322799682617 952.377197265625 61.52242660522461">
+                        </path>
+                    </svg>
+                    <svg className="Ellipse_439">
+                        <ellipse id="Ellipse_439" rx="0.5707299709320068" ry="0.5707299709320068"
+                                 cx="0.5707299709320068" cy="0.5707299709320068">
+                        </ellipse>
+                    </svg>
+                    <svg className="Ellipse_440">
+                        <ellipse id="Ellipse_440" rx="1.1495554447174072" ry="1.1495554447174072"
+                                 cx="1.1495554447174072" cy="1.1495554447174072">
+                        </ellipse>
+                    </svg>
+                </div>
+                <svg className="Ellipse_441">
+                    <ellipse id="Ellipse_441" rx="10" ry="10" cx="10" cy="10">
+                    </ellipse>
+                </svg>
+            </div>
+            <div id="Group_2285">
+                <svg className="Ellipse_685">
+                    <ellipse id="Ellipse_685" rx="57.5" ry="57.5" cx="57.5" cy="57.5">
+                    </ellipse>
+                </svg>
+                <div id="Menu" className="Menu">
+                    <div id="Group_457">
+                        <svg className="Line_42" viewBox="0 0 15.124 2">
+                            <path id="Line_42" d="M 0 0 L 15.12399291992188 0">
+                            </path>
+                        </svg>
+                        <svg className="Line_43" viewBox="0 0 15.124 2">
+                            <path id="Line_43" d="M 0 0 L 15.12399291992188 0">
+                            </path>
+                        </svg>
+                        <svg className="Line_44" viewBox="0 0 15.124 2">
+                            <path id="Line_44" d="M 0 0 L 15.12399291992188 0">
+                            </path>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        </body>
+    )
+}
+    export default App;
