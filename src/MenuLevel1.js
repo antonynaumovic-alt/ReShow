15d46aa26d14ad669ce9a768589e65ca77f34e78
amymullins1 <19016241@brookes.ac.uk>
2021-02-09 10:46:55 +0000

    Menu Level 1

---
+++ src/MenuLevel1.js
@@ -0,0 +1,187 @@
+import React, { Component } from 'react';
+import logo from './Enrolle_cy.png';
+import logo2 from './Enrolle_cy@2x.png'
+import loginIcon from './login_icon.png'
+import './MenuLevel1.css';
+
+function App() {
+    return (
+        <body>
+        <div id="ID1600-08-MenuLevel1">
+            <div id="BG">
+                <svg className="Orange" viewBox="0 0 1133 960">
+                    <path id="Orange" d="M 0 0 L 1133 0 L 1133 960 L 0 960 L 0 0 Z">
+                    </path>
+                </svg>
+                <svg className="Off_white" viewBox="-3460.713 -8512.411 1024 960.351">
+                    <path id="Off_white"
+                          d="M -2436.71337890625 -7552.0615234375 C -2436.71337890625 -7552.0615234375 -2655.979736328125 -8008.181640625 -2664.87841796875 -8512.0615234375 C -2664.44384765625 -8513.0537109375 -3460.712890625 -8511.619140625 -3460.712890625 -8511.619140625 L -3460.712890625 -7552.060546875 L -2436.71337890625 -7552.0615234375 Z">
+                    </path>
+                </svg>
+            </div>
+            <div id="Featured_Events">
+                <div id="Featured_Events_">
+                    <span>Featured Events</span>
+                </div>
+                <div id="Event_Card_1" className="Event_Card_1">
+                    <svg className="Rectangle_31">
+                        <rect id="Rectangle_31" rx="10" ry="10" x="0" y="0" width="400" height="300">
+                        </rect>
+                    </svg>
+                    <svg className="ID1_1">
+                        <rect id="ID1_1" rx="10" ry="10" x="0" y="0" width="400" height="300">
+                        </rect>
+                    </svg>
+                    <div id="Date">
+                        <div id="ID29_Apr">
+                            <span>DATE</span>
+                        </div>
+                    </div>
+                    <div id="WebRTC_Upgrades">
+                        <span>WebRTC Upgrades</span>
+                    </div>
+                </div>
+                <div id="Event_Card_2" className="Event_Card_2">
+                    <svg className="Rectangle_31_">
+                        <rect id="Rectangle_31_" rx="10" ry="10" x="0" y="0" width="400" height="300">
+                        </rect>
+                    </svg>
+                    <svg className="DP_List_a_1">
+                        <rect id="DP_List_a_1" rx="10" ry="10" x="0" y="0" width="400" height="300">
+                        </rect>
+                    </svg>
+                    <div id="Date_">
+                        <div id="ID29_Apr_">
+                            <span >DATE</span>
+                        </div>
+                    </div>
+                    <div id="Server_Maintenance">
+                        <span>Server Maintenance</span>
+                    </div>
+                </div>
+                <div id="Event_Card_2_" className="Event_Card_2">
+                    <svg className="Rectangle_31_ba">
+                        <rect id="Rectangle_31_ba" rx="10" ry="10" x="0" y="0" width="400" height="300">
+                        </rect>
+                    </svg>
+                    <svg className="DP_List_a_1_bb">
+                        <rect id="DP_List_a_1_bb" rx="10" ry="10" x="0" y="0" width="400" height="300">
+                        </rect>
+                    </svg>
+                    <div id="Date_bc">
+                        <div id="ID29_Apr_bd">
+                            <span>DATE</span>
+                        </div>
+                    </div>
+                    <div id="Server_Maintenance_be">
+                        <span>Server Maintenance</span>
+                    </div>
+                </div>
+                <div id="Button" className="Calendar_Btn">
+                    <button className="Button">Full Calendar</button>
+                </div>
+                <div id="Left_arrow" className="LArrow_Btn">
+                    <button className = "Left_arrow">hello</button>
+
+                    <svg className="Left_Pointing_Arrow" viewBox="0 0 16 12">
+                        <path id="Left_Pointing_Arrow"
+                              d="M 6.335898399353027 2.496150493621826 C 7.127542495727539 1.308684587478638 8.872455596923828 1.308684587478638 9.66409969329834 2.496150493621826 L 13.92706489562988 8.890597343444824 C 14.81313896179199 10.21970748901367 13.86035537719727 11.99999809265137 12.26296424865723 11.99999809265137 L 3.737034320831299 11.99999809265137 C 2.13964319229126 11.99999809265137 1.18686056137085 10.21970748901367 2.072933673858643 8.890597343444824 Z">
+                        </path>
+                        </svg>
+                </div>
+                <div id="Right_arrow" className="RArrow_btn">
+                    <button className = "Right_arrow">hello</button>
+                       {/* <svg className="Ellipse_429_bo">
+                            <ellipse id="Ellipse_429_bo" rx="20" ry="20" cx="20" cy="20">
+                            </ellipse>
+                        </svg>*/}
+                    <svg className="Right_Pointing_Arrow" viewBox="0 0 16 12">
+                        <path id="Right_Pointing_Arrow"
+                              d="M 6.335898399353027 2.496150493621826 C 7.127542495727539 1.308684587478638 8.872455596923828 1.308684587478638 9.66409969329834 2.496150493621826 L 13.92706489562988 8.890597343444824 C 14.81313896179199 10.21970748901367 13.86035537719727 11.99999809265137 12.26296424865723 11.99999809265137 L 3.737034320831299 11.99999809265137 C 2.13964319229126 11.99999809265137 1.18686056137085 10.21970748901367 2.072933673858643 8.890597343444824 Z">
+                        </path>
+                    </svg>
+                </div>
+            </div>
+
+            <div id="Contact_Btn">
+                <div id="Contact">
+                    <button className= "Contact">Contact</button>
+                </div>
+
+            </div>
+
+                <div id="Logins">
+                    <div id="Logins_b">
+                        <button className ="Logins_b">Login</button>
+                    </div>
+                </div>
+            <div id="Group_2286">
+                <div id="Group_2285">
+                    <svg className="Ellipse_685">
+                        <ellipse id="Ellipse_685" rx="75" ry="75" cx="75" cy="75">
+                        </ellipse>
+                    </svg>
+                </div>
+                <div onClick="application.goToTargetView(event)" id="Menu">
+                    <div id="Group_457">
+                        <svg className="Line_43" viewBox="0 0 15.858 15.858">
+                            <path id="Line_43" d="M 0 0 L 15.85791015625 15.85791015625">
+                            </path>
+                        </svg>
+                        <svg className="Line_44" viewBox="0 0 15.858 15.79">
+                            <path id="Line_44" d="M 0 15.78951263427734 L 15.85791015625 0">
+                            </path>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+            <div id="Level_1">
+                <div id="Our_Stories">
+                    <div id="FAQ">
+                        <button className= "FAQ_Btn">FAQ</button>
+                    </div>
+                </div>
+                <div id="Work_at_ISU">
+                    <div id="Implementations">
+                        <button className="Implementation_Btn">Implementation</button>
+                    </div>
+                </div>
+                <div id="ISU_Community">
+                    <div id="Why_Us">
+                        <button className = "Why_Us_Btn">Why Us?</button>
+                    </div>
+                </div>
+                <div id="Beyond_the_classroom">
+                    <div id="ApplicationBtn">
+                        <button className= "ApplicationBtn">Application</button>
+                    </div>
+                </div>
+                <div id="Learning_at_ISU">
+                    <div id="MissionValuesBtn">
+                        <button className={"MissionValuesBtn"}>Mission & Values</button>
+                    </div>
+                </div>
+                <div id="Our_School">
+                    <div id="FeaturesBtn" className="Features">
+                        <button className="FeaturesBtn">Features</button>
+                    </div>
+                </div>
+
+            <div id="HomeBtn" className = "Home">
+                <button className = "HomeBtn">home</button>
+            </div>
+            </div>
+
+            <div id="Enrolle" className="Enrolle">
+                <div id="Enrolle_cx">
+                    <img id="Enrolle_cy" src={logo}>
+                    </img>
+                </div>
+            </div>
+        </div>
+
+        </body>
+
+    )
+}
+    export default App;
