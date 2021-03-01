1e359befabe1b8fdeb1beb927f6d48072e917a75
Antony <19042535@brookes.ac.uk>
2021-03-01 10:22:23 +0000

    react compilation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 import './App.css';
+
+
+////////////////////////////////
+
+import Navbar from './components/Navbar';
+import Menu from './components/Menu';
+import Homepage from './components/Homepage';
+import Login from './components/Login';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Menu />
+        <Switch>
+          <Route path='/' exact component={Homepage}/>
+          <Route path='/login' exact component={Login}/>
+        </Switch>
+      </Router>
+
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import EnrolleLogo from './Enrolle.png';
+import './Navbar.css';
+
+function Navbar() {
+
+
+    return (
+        <>
+            
+        
+
+        </>
+    )
+}
+
+export default Navbar
