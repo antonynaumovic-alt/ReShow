9a4733e0a639635f836f77fdb080be873fd385a9
elliottjd <45365311+elliottjd@users.noreply.github.com>
2021-04-05 15:22:37 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,92 @@
+<?php
+
+session_start();
+$_SESSION['name'] = "User";
+if(isset($_GET['logout'])){
+
+
+    $logout_message = "<div class='msgln'><span class='left-info'>User <b class='user-name-left'>". $_SESSION['name'] ."</b> has left the chat session.</span><br></div>";
+    file_put_contents("log.html", $logout_message, FILE_APPEND | LOCK_EX);
+
+    session_destroy();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+
+        <title>chat test</title>
+
+
+    </head>
+    <body>
+    <?php
+
+    ?>
+
+        <div id="wrapper">
+            <div id="menu">
+                <p class="welcome">Welcome, <b><?php echo $_SESSION['name']; ?></b></p>
+                <p class="logout"><a id="exit" href="#">Exit Chat</a></p>
+            </div>
+
+            <div id="chatbox">
+            <?php
+            if(file_exists("log.html") && filesize("log.html") > 0){
+                $contents = file_get_contents("log.html");
+                echo $contents;
+            }
+            ?>
+            </div>
+
+            <form name="message" action="">
+                <input name="usermsg" type="text" id="usermsg" />
+                <input name="submitmsg" type="submit" id="submitmsg" value="Send" />
+            </form>
+        </div>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script type="text/javascript">
+
+            $(document).ready(function () {
+                $("#submitmsg").click(function () {
+                    var clientmsg = $("#usermsg").val();
+                    $.post("post.php", { text: clientmsg });
+                    $("#usermsg").val("");
+                    return false;
+                });
+
+                function loadLog() {
+                    var oldscrollHeight = $("#chatbox")[0].scrollHeight - 20;
+
+                    $.ajax({
+                        url: "log.html",
+                        cache: false,
+                        success: function (html) {
+                            $("#chatbox").html(html);
+
+
+                            var newscrollHeight = $("#chatbox")[0].scrollHeight - 20;
+                            if(newscrollHeight > oldscrollHeight){
+                                $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal');
+                            }
+                        }
+                    });
+                }
+
+                setInterval (loadLog, 2500);
+
+                $("#exit").click(function () {
+                    var exit = confirm("Are you sure you want to end the session?");
+                    if (exit == true) {
+                    window.location = "index.php?logout=true";
+                    }
+                });
+            });
+        </script>
+    </body>
+</html>
+<?php
+?>
